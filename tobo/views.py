7111e826d9d22b5310d480f7b1b34b859741bd84
1F10230282 <s1f102302821@iniad.org>
2024-07-03 21:27:59 +0900

    トップページを追加

--- tobo/views.py
+++ tobo/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.utils.dateparse import parse_datetime
+from tobo.models import Task
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        task = Task(title=request.POST['title'],due_at=make_aware(parse_datetime(request.POST['due_at'])))
+        
+        task.save()
+
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks': tasks
+    }
+    return render(request, 'tobo/index.html', context)
